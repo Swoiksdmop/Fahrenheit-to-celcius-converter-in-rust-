cadd86742193715b4756d33f851859acbc1fef4a
Darell Ethan Kiganga <kigangadarell@gmail.com>
2023-06-11 18:32:38 +0300

    Added floats

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+fn main() {
+  println!("Welcome to Ethan's program of converting Fahrenheit to Celcius!!!\n\nWhat would you like to convert to celcius? >");
+
+  let mut one = String::new();
+
+  io::stdin().read_line(&mut one).expect("Failed to read line");
+
+  let one: f64 = one.trim().parse::<f64>().expect("Failed to read line");
+  
+  let _conversion = one - 32.0;
+  let _conversion2 = _conversion * 5.0;
+  let _conversion3 = _conversion2 / 9.0;
+
+  println!("\n\n{one} degrees fahrenheit in celcius is {_conversion3} degrees celcius");
+}
